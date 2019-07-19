a77f8db134b93872be7120523a42db2571b9ed66
Ricardo da Silva <ricardolopes18@yahoo.com.br>
2019-07-19 20:50:35 +0200

    Correct some print statements

--- exercise2/exercise2.py
+++ exercise2/exercise2.py
@@ -8,8 +8,10 @@ def format_filename():
     return ""+str(current_time.year)+"-"+str(current_time.month)+"-"+str(current_time.day)+"_"+str(current_time.hour)+"-"+str(current_time.minute)+"-"+str(current_time.second)
 
 def write_to_JSON(data):
-    with open("{}.json".format(format_filename()), "w") as json_file:
+    file_name = "{}.json".format(format_filename())
+    with open(file_name, "w") as json_file:
         json.dump(data, json_file)
+    print("The content was saved to a file called: {}\n".format(file_name))
 
 def multiplication(numbers):
     multi = 1
@@ -27,10 +29,15 @@ def multiplication(numbers):
         multi = int(number) * multi
 
     multiplication_data["Multiplication"] = multi
+    print("Printing your JSON below:")
+    print(json.dumps(multiplication_data, indent=4, sort_keys=True))
     write_to_JSON(multiplication_data)
 
+def subtract(num):
+    return int(num) - int(num)
 def subtraction(numbers):
-    return reduce(lambda x,y: x-y, numbers)
+    sub = map(subtract, numbers)
+    return sub
 
 def ramdon_number(numbers):
     pass
@@ -40,26 +47,31 @@ def sorted_reverse(numbers):
     pass
 
 def menu(numbers):
-    print("The numbers you've selected are: {}".format(numbers))
-    print("""
+    numbers_selected = "The numbers you've selected are: {}".format(numbers)
+    options = """
+    
     1 - Multiply, show result and write to JSON file
     2 - Subtract, show result and write to JSON file
     3 - Show random number
     4 - Show sorted list (from lower to highest)
-    5 - Show reverse sorted list (from highest to lower)""")
-    chosen = raw_input("Please, select one:")
-    if chosen =='1': 
-        print "add" 
-    elif chosen == '2': 
-        print(subtraction(numbers))
-    elif chosen == '3':
-        print "find" 
-    elif chosen == '4': 
-        print("Show")
-    elif chosen == '5': 
-        print ("Show")
-    else: 
-        print "Unknown Option Selected!" 
+    5 - Show reverse sorted list (from highest to lower)\n"""
+    while True:
+        print(numbers_selected, options)
+        chosen = input("Please, select one:")
+        if chosen =='1': 
+            multiplication(numbers)
+        elif chosen == '2': 
+            for s in map(lambda x: int(x)-int(x), numbers):
+                print(s, end=",")
+            print("\n")
+        elif chosen == '3':
+            pass
+        elif chosen == '4': 
+            print("Show")
+        elif chosen == '5': 
+            print ("Show")
+        else: 
+            print("Unknown Option Selected!")
 
 if __name__ == "__main__":
     try:
