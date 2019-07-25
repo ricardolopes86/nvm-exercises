08eb0dc87ba288b0a5e37c82ae35c4110ef7a79b
Ricardo Silva <ricardo-za.silva@ubs.com>
2019-07-25 17:07:53 +0200

    Implementing cleanin and package

--- exercise2/cleanup.py
+++ exercise2/cleanup.py
@@ -4,7 +4,22 @@ Script to clean our environment.
 import os
 
 def main():
-    echo "Cleaning up the directories..."
+    print("Cleaning up the directories...")
+    for folder in ["build/", "test/", "package/"]:
+        if os.path.exists(folder):
+            for root, dirs, files in os.walk(folder, topdown=False):
+                for name in files:
+                    print("Removing '{}'".format(os.path.join(root, name)))
+                    os.remove(os.path.join(root, name))
+                for name in dirs:
+                    print("Removing '{}'".format(os.path.join(root, name)))
+                    os.rmdir(os.path.join(root, name))
+            print("Removing '{}'".format(folder))
+            os.rmdir(folder)
+        else:
+            print("No folder '{}' was found...".format(folder))
+            
+    print("Over and out!")
 
 if __name__ == "__main__":
     main()
--- exercise2/package.py
+++ exercise2/package.py
@@ -1,10 +1,42 @@
 """
 Script that will package our application to be delivered.
 """
-import os
+import os, sys
+import tarfile
 
-def main():
+def create_tar(tar_filename):
+    """
+    Function for checking if built artifact is present and tar it.
+    """
+    if os.path.exists("build/app.pyc"):
+        with tarfile.open("package/{}.tar.gz".format(tar_filename), "w:gz") as tar:
+            tar.add("build/app.pyc")
+        tar.close()
+        print("Done... Over and Out!")
+    else:
+        print("Compiled application wasn't found, please, build it before proceeding...")
+
+def main(tar_filename):
+    """
+    Prepare the package directory and call the tar function.
+    """
     print("Packaging our application...")
+    if not os.path.exists("package/"):
+        print("Creating missing 'package' directory...")
+        os.mkdir("package/")
+    else:
+        print("Packaging directory exists, cleaning it up...")
+        if os.path.exists("package/"):
+            for root, dirs, files in os.walk("package/", topdown=False):
+                for name in files:
+                    os.remove(os.path.join(root, name))
+                for name in dirs:
+                    os.rmdir(os.path.join(root, name))
+
+    create_tar(tar_filename)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) <= 1:
+        print("Missing argument. Please, provide the package name, without the .tar.gz, just a word.")
+    else:
+        main(sys.argv[1])
